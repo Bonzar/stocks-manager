8ca81e70b72848d79126740b83af0d8a11de77e0
bonzar <nva112002@gmail.com>
2023-10-11 20:23:15 +0300

    use idParams in idParamsSchema

--- src/api/schemas/idParamsSchema.ts
+++ src/api/schemas/idParamsSchema.ts
@@ -1,12 +1,10 @@
 import { FromSchema, JSONSchema } from "json-schema-to-ts";
+import { idSchema } from "./idSchema.js";
 
 export const idParamsSchema = {
   type: "object",
-  properties: {
-    id: { type: "number" },
-  },
-  additionalProperties: false,
+  properties: { id: idSchema },
   required: ["id"],
 } as const satisfies JSONSchema;
 
-export type IIdParamsSchema = FromSchema<typeof idParamsSchema>;
+export type IIdParamsSchema = RemoveIndex<FromSchema<typeof idParamsSchema>>;
